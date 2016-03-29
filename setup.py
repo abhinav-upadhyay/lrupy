705d3088a678f72d0de77d1c33e206160bee6421
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2016-03-30 02:54:32 +0530

    Add download URL, also fix license

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
     name = "lrupy",
     packages = ['lrupy'],
-    version = "0.1",
-    py_modules=['lrupy'],
+    version = "0.2",
+    py_modules=['lrupy.lrupy'],
     description = "A simple in-memory (LRU) cache implementation",
     author = "Abhinav Upadhyay",
     author_email = "er.+abhinav.+updadhyay@gmail.com",
     url = "https://github.com/abhinav-upadhyay/lrupy",
+    download_url="https://github.com/abhinav-upadhyay/lrupy/archive/0.2.tar.gz",
     keywords=['development', 'cache', 'performance'],
     classifiers = [
         "Programming Language :: Python :: 2.6",
@@ -16,9 +17,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: BSD",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Python Modules"
         ],
     long_description=open('README.md').read())
 
