88d3cc62c37805463caf5c81ad62cecdaa4ff04b
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2016-03-30 03:25:10 +0530

    Add MANIFEST.in to incude README and LICENSE in the release.
    Add license to the setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name = "lrupy",
-    packages = ['lrupy'],
-    version = "0.2",
+    name="lrupy",
+    packages=['lrupy'],
+    version="0.2",
     py_modules=['lrupy.lrupy'],
-    description = "A simple in-memory (LRU) cache implementation",
-    author = "Abhinav Upadhyay",
-    author_email = "er.+abhinav.+updadhyay@gmail.com",
-    url = "https://github.com/abhinav-upadhyay/lrupy",
+    description="A simple in-memory (LRU) cache implementation",
+    author="Abhinav Upadhyay",
+    author_email="er.+abhinav.+updadhyay@gmail.com",
+    url="https://github.com/abhinav-upadhyay/lrupy",
     download_url="https://github.com/abhinav-upadhyay/lrupy/archive/0.2.tar.gz",
     keywords=['development', 'cache', 'performance'],
-    classifiers = [
+    license='BSD',
+    classifiers=[
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
