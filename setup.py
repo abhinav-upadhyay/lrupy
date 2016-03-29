3c3783e82fa604dcb025d5d54473109841d6f70c
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2016-03-30 03:36:23 +0530

    Bump version as the package is still not pip installable because of missing README.md from the tar

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name="lrupy",
     packages=['lrupy'],
-    version="0.3",
+    version="0.4",
     py_modules=['lrupy.lrupy'],
     description="A simple in-memory (LRU) cache implementation",
     author="Abhinav Upadhyay",
     author_email="er.+abhinav.+updadhyay@gmail.com",
     url="https://github.com/abhinav-upadhyay/lrupy",
-    download_url="https://github.com/abhinav-upadhyay/lrupy/archive/0.3.tar.gz",
+    download_url="https://github.com/abhinav-upadhyay/lrupy/archive/0.4.tar.gz",
     keywords=['development', 'cache', 'performance'],
     license='BSD',
     classifiers=[
